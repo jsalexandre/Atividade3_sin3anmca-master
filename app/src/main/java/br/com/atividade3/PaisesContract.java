0745d82e006c4ce6622edba9facdfcbeec9448ad
arqdsis <arqdsis@moclii0309.animaedu.intranet>
2018-03-20 20:59:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/atividade3/PaisesContract.java
@@ -0,0 +1,20 @@
+package br.com.atividade3;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Jéssica Alexandre
+ */
+public class PaisesContract {
+
+    public static abstract class PaisEntry implements BaseColumns {
+        public static final String TABLE_NAME = "pais";
+        public static final String COLUMN_NAME_NOME = "nome";
+        public static final String COLUMN_NAME_REGIAO = "regiao";
+        public static final String COLUMN_NAME_CAPITAL = "capital";
+        public static final String COLUMN_NAME_BANDEIRA = "bandeira";
+        public static final String COLUMN_NAME_CODIGO3 = "codigo3";
+
+
+    }
+}
